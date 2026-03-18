6370e84dc46e0e0ecb3cc68f43dd7cfc0aaf082f
IceyWu <3128006406@qq.com>
2026-03-18 16:11:45 +0800

    feat: 添加 remapExamplesToRoot 函数以重新映射示例并更新根示例列表

--- apps/playground/src/scenarios/utils.ts
+++ apps/playground/src/scenarios/utils.ts
@@ -4,13 +4,38 @@ import { downloadExamples, logExamples, networkExamples } from "./browser";
 import { isExamples } from "./is";
 import { lodashLiteExamples } from "./lodash-lite";
 import { objectExamples } from "./object";
-import { promiseExamples, rootExamples, toProExamples } from "./promise";
+import { promiseExamples, toProExamples } from "./promise";
 import { sharedExamples } from "./shared";
 import { toolsExamples } from "./tools";
+import type { PlaygroundExample } from "../types";
 
 function createKey(importPath: string, exportName: string) {
   return `${importPath}:${exportName}`;
 }
+
+function remapExamplesToRoot(
+  examples: readonly PlaygroundExample[]
+): PlaygroundExample[] {
+  return examples.map((example) => ({
+    ...example,
+    code: example.code.split(example.importPath).join("@iceywu/utils"),
+    importPath: "@iceywu/utils",
+  }));
+}
+
+const rootExamples = remapExamplesToRoot([
+  ...promiseExamples,
+  ...arrayExamples,
+  ...asyncTaskExamples,
+  ...objectExamples,
+  ...isExamples,
+  ...sharedExamples,
+  ...downloadExamples,
+  ...lodashLiteExamples,
+  ...logExamples,
+  ...networkExamples,
+  ...toolsExamples,
+]);
 
 const exampleList = [
   ...rootExamples,
